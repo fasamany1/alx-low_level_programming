dde4ed1cf1e0fa093e10f4104f420690c5f33c85
Forster Asamany <asamanyforster@yahoo.com>
2022-09-21 07:36:25 -0700

    Write a function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,38 +1,28 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string.
- * @ch: Pointer to Char
+ * leet - encodes a string into 1337.
+ * @s: Pointer to Char
  * Return: char.
  */
 
-char *cap_string(char *ch)
+char *leet(char *s)
 {
 int i = 0;
+int j = 0;
+char *l = "aAeEoOlLtT";
+char *n = "4433001177";
 
-while (*(ch + i) != '\0')
+while (*(s + i) != '\0')
 {
-if (i == 0)
-*(ch + i) = *(ch + i) - ' ';
-if (*(ch + i) == ' ' || *(ch + i) == '\t')
-i++;
-else if (*(ch + i) == '\n' || *(ch + i) == ',')
-i++;
-else if (*(ch + i) == ';' || *(ch + i) == '.')
-i++;
-else if (*(ch + i) == '!' || *(ch + i) == '?')
-i++;
-else if (*(ch + i) == '"' || *(ch + i) == '(')
-i++;
-else if (*(ch + i) == ')' || *(ch + i) == '{')
-i++;
-else if (*(ch + i) == '}')
-i++;
-if (*(ch + i) >= 97 && *(ch + i) <= 122)
+while (*(l + j) != '\0')
 {
-*(ch + i) = *(ch + i) -' ';
+if (*(s + i) == *(l + j))
+*(s + i)  = *(n + j);
+j++;
+}
+j = 0;
 i++;
 }
-}
-return (ch);
+return (s);
 }
