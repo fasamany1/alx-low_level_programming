e665a3d579df930589641eb4d0a0c0d11cb246e7
Forster Asamany <asamanyforster@yahoo.com>
2022-10-11 04:25:43 +0000

    Write a function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,76 +1,52 @@
 #include "dog.h"
-
+#include <stdlib.h>
 /**
- * _strdup - create a new array containing a copy of the given string
- * @str: a pointer to the string to copy
- *
- * Return: NULL if str is NULL or if memory allocation fails,
- * otherwise a return a pointer to the new copy
+ * new_dog -  creates a new dog.
+ * @name: char type
+ * @age: float type
+ * @owner: char type
+ * Return: Pointer to struct
  */
-char *_strdup(char *str)
-{
-	char *dup;
-	unsigned int size = 0;
 
-	if (str)
-	{
-		while (str[size++])
-			;
-
-		dup = malloc(sizeof(char) * size);
-		if (dup)
-		{
-			while (size--)
-				dup[size] = str[size];
-
-			return (dup);
-		}
-	}
-	return (NULL);
-}
-
-/**
- * new_dog - create a new dog
- * @name: the new dog's name
- * @age: the new dog's age
- * @owner: the new dog's owner
- *
- * Return: a pointer to the new dog, or NULL if memory allocation fails
- */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d;
+	dog_t *newDog;
+	int length1, length2, i, j;
 
-	d = malloc(sizeof(dog_t));
-	if (!d)
+	length1 = 0, length2 = 0;
+	newDog = malloc(sizeof(struct dog));
+
+	if (newDog == NULL)
 		return (NULL);
 
-	if (name)
+	while (name[length1++])
+		;
+	while (owner[length2++])
+		;
+
+	newDog->name = malloc(sizeof(newDog->name) * length1);
+
+	if (newDog->name == NULL)
 	{
-		d->name = _strdup(name);
-		if (!(d->name))
-		{
-			free(d);
-			return (NULL);
-		}
+		free(newDog);
+		return (NULL);
 	}
-	else
-		d->name = NULL;
 
-	d->age = age;
+	for (i = 0; i < length1; i++)
+		newDog->name[i] = name[i];
 
-	if (owner)
+	newDog->age = age;
+
+	newDog->owner = malloc(sizeof(newDog->owner) * length2);
+
+	if (newDog->owner == NULL)
 	{
-		d->owner = _strdup(owner);
-		if (!(d->owner))
-		{
-			free(d->name);
-			free(d);
-			return (NULL);
-		}
+		free(newDog->name);
+		free(newDog);
+		return (NULL);
 	}
-	else
-		d->owner = NULL;
+	for (j = 0; j < length2; j++)
+		newDog->owner[j] = owner[j];
 
-	return (d);
+	return (newDog);
 }
